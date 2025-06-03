14a038559dccd9d337e03640f5ee866b1998b7e4
dansheehan5 <danielsheehan527@gmail.com>
2025-06-03 11:15:16 +1000

    fixed segfault

--- include/lions/firewall/cuckoo_table.c
+++ include/lions/firewall/cuckoo_table.c
@@ -12,7 +12,7 @@ uint32_t MAX_ITERS = 11;
 
 typedef struct hash_table {
     uint16_t (*hash)(uint16_t);
-    uint16_t entries[11];
+    uint16_t *entries[11];
 } hash_table_t;
 
 typedef struct cuckoo_table {
@@ -30,18 +30,18 @@ uint16_t hash2(uint16_t key) {
 
 uint16_t *get_entry(cuckoo_table_t *cuckoo_table, uint16_t *entry) {
     hash_table_t t1 = cuckoo_table->tables[0];
-    uint16_t (*h1)(uint16_t) = t1.hash;
+    uint16_t h1 = t1.hash(*entry);
 
-    uint16_t *found = &t1.entries[h1(*entry)];
+    uint16_t *found = t1.entries[h1];
     
     if (found != NULL && found == entry) {
         return found;
     }
 
     hash_table_t t2 = cuckoo_table->tables[1];
-    uint16_t (*h2)(uint16_t) = t2.hash;
+    uint16_t h2 = t2.hash(*entry);
 
-    found = &t2.entries[h2(*entry)];
+    found = t2.entries[h2];
 
     if (found != NULL && found == entry) {
         return found;
@@ -55,39 +55,41 @@ void evict(cuckoo_table_t *cuckoo_table) {
 }
 
 void insert(cuckoo_table_t *cuckoo_table, uint16_t *entry) {
+
     if (get_entry(cuckoo_table, entry) != NULL) {
         return;
     }
-    
-    printf("bla\n");
 
-    hash_table_t t1 = cuckoo_table->tables[0];
-    hash_table_t t2 = cuckoo_table->tables[1];
-    printf("yabadabado\n");
+    hash_table_t *t1 = &cuckoo_table->tables[0];
+    hash_table_t *t2 = &cuckoo_table->tables[1];
 
     uint16_t *x = entry;
     
-    uint16_t h1 = t1.hash(*x);
-    uint16_t h2 = t2.hash(*x);
+    uint16_t h1 = t1->hash(*x);
+    uint16_t h2 = t2->hash(*x);
 
     for (int i = 0; i < MAX_ITERS; i++) {
-        if (&t1.entries[h1] == NULL) {
-            t1.entries[h1] = *x;
+        if (t1->entries[h1] == NULL) {
+            t1->entries[h1] = entry;
+            printf("placed %d in t1 at index %d\n", *x, h1);
+            printf("%d\n", *(t1->entries[h1]));
             return;
         }
 
         uint16_t *tmp = x;
-        x = &t1.entries[h1];
-        t1.entries[h1] = *tmp;
+        x = t1->entries[h1];
+        t1->entries[h1] = tmp;
 
-        if (&t2.entries[h2] == NULL) {
-            t2.entries[h2] = *x;
+        printf("%p\n", t2->entries[h2]);
+        if (t2->entries[h2] == NULL) {
+            t2->entries[h2] = entry;
+            printf("placed %d in t2 at index %d\n", *entry, h2);
             return;
         }
 
         tmp = x;
-        x = &t2.entries[h2];
-        t2.entries[h2] = *tmp;
+        x = t2->entries[h2];
+        t2->entries[h2] = tmp;
     }
 
     evict(cuckoo_table);
@@ -106,6 +108,10 @@ void update(cuckoo_table_t *cuckoo_table, uint16_t *new_entry) {
 }
 
 void init_ht(hash_table_t *t, uint16_t (*hash)(uint16_t)) {
+    for (int i = 0; i < 11; i++) {
+        t->entries[i] = NULL;
+    }
+
     t->hash = hash;
 }
 
@@ -132,7 +138,8 @@ int main(int argc, char *argv[]) {
     uint16_t in = 100;
 
     insert(test, &in);
-    printf("%d\n", *(get_entry(test, &in)));
+    uint16_t *found = get_entry(test, &in);
+    printf("%d\n", *found);
 
 
 
